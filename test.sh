ca7f34aba7a066821e5977d3fd84f5bf4f963eb6
Alexander F Rødseth <rodseth@gmail.com>
2017-11-20 19:26:15 +0100

    Remove check and test script

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-if [ ! -x ./cdetect ]; then
-  echo 'Could not find executable: cdetect'
-  echo 'Has it been built with "go build"?'
-  exit 1
-fi
-for f in /usr/bin/*; do echo -n "$f: "; ./cdetect "$f"; done
